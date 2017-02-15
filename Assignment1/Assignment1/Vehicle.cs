af2e3d781fa94bf58af7a4f1f4de4e6bd4ce4ab4
jamie <jamie@JAMIE-LAPTOP>
2017-02-15 12:01:29 +0000

    +Started Program +Work on Vehicle Class and xaml for mainWindow - Some xaml broken

---
+++ Assignment1/Assignment1/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+        public DateTime Year { get; set; }
+        public string Colour { get; set; }
+        public int Mileage { get; set; }
+        public string Description { get; set; }
+    }
+}
